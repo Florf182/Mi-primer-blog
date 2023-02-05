e9cbd4523fe14babc17e80de2bdc37483b9f8531
Florf182 <118774820+Florf182@users.noreply.github.com>
2023-02-05 16:46:53 -0300

    Delete blog_practice directory

--- blog_practice/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-# Register your models here.
-@admin.register(Post)
-class Post_admin(admin.ModelAdmin):
-    list_display = ['title', 'text', 'created_at', 'updated_at']
